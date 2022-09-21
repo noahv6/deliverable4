2c069d15064e36ffb135347d6f47cd24d8a87e4a
noahv6 <111916853+noahv6@users.noreply.github.com>
2022-09-20 21:49:26 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace deliverable_4 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int next = 0;
+            int previous = 0;
+
+            for(int i = 0; i <= 24; i++)
+            {
+                Console.WriteLine("Fibonacci number " + "(" + i + ") " + "is " + next);
+                if (next == 0)
+                {
+                    next = 1;
+
+                }
+                else
+                {
+                    int now = next;
+                    next = next + previous;
+                    previous = now;
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
